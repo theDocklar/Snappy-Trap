4312b968a133ee2946e8c8fb4dbbc7bc56a4ac35
Tharindu Mishen <tharindumishen@gmail.com>
2026-07-20 20:48:54 +0530

    Add featured products section to ProductsHero component and update product packaging images in the products library

--- lib/products.ts
+++ lib/products.ts
@@ -36,6 +36,9 @@ export const products: Product[] = [
       "/images/dk105-6.jpg",
       "/images/dk105-7.jpg",
       "/images/dk105-8.jpg",
+      "/images/dk105-9.png",
+      "/images/dk105-10.png",
+      "/images/dk105-11.jpg",
     ],
     bulletFeatures: [
       "Everything you need in one complete kit",
@@ -76,6 +79,9 @@ export const products: Product[] = [
       "/images/dk100-6.jpg",
       "/images/dk100-7.jpg",
       "/images/dk100-8.jpg",
+      "/images/dk100-11.jpg",
+      "/images/dk100-9.png",
+      "/images/dk100-10.png",
     ],
     bulletFeatures: [
       "Everything you need in one complete kit",
@@ -118,6 +124,8 @@ export const products: Product[] = [
       "/images/dk110-3.jpg",
       "/images/dk110-4.jpg",
       "/images/dk110-5.jpg",
+      "/images/dk110-6.png",
+      "/images/dk110-7.png",
     ],
     bulletFeatures: [
       "Everything you need in one complete kit",
